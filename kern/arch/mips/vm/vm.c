5463b051f613e21b8aceaae2d3472fc8576443fd
dustlee <dustlee@uw.edu>
2014-11-29 14:24:30 -0800

    Finished implementing vm_fault for STEP1

--- kern/arch/mips/vm/vm.c
+++ kern/arch/mips/vm/vm.c
@@ -5,6 +5,9 @@
 #include <mips/tlb.h>
 #include <spl.h>
 #include <vm.h>
+#include <addrspace.h>
+#include <current.h>
+#include <proc.h>
 
 void
 vm_tlbshootdown_all(void)
@@ -12,94 +15,87 @@ vm_tlbshootdown_all(void)
     int spl = splhigh();
     
     int i;
-	for (i=0; i<NUM_TLB; i++) {
-		tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
-	}
+    for (i=0; i<NUM_TLB; i++) {
+        tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
+    }
 
-	splx(spl);
+    splx(spl);
 }
 
 void
 vm_tlbshootdown(const struct tlbshootdown *ts)
 {
-	(void)ts;
-	vm_tlbshootdown_all();
+    (void)ts;
+    vm_tlbshootdown_all();
 }
-/*
+
 int
 vm_fault(int faulttype, vaddr_t faultaddress)
 {
     if (curproc == NULL) {
-*/		/*
-		 * No process. This is probably a kernel fault early
-		 * in boot. Return EFAULT so as to panic instead of
-		 * getting into an infinite faulting loop.
-		 */
-/*		return EFAULT;
-	}
+        /*
+         * No process. This is probably a kernel fault early
+         * in boot. Return EFAULT so as to panic instead of
+         * getting into an infinite faulting loop.
+         */
+        return EFAULT;
+    }
 
-	as = proc_getas();
-	if (as == NULL) {
-*/		/*
-		 * No address space set up. This is probably also a
-		 * kernel fault early in boot.
-fault		 */
-/*		return EFAULT;
-	}
-	for(i=0;i<4;i++) 
-	  {
-	    
-	    if(faultaddress >= as->segment_table[i].start && faultaddress < segment_table[i].end)
-	      {
-		if(!as->ignore_permissions) 
-		  {
-		    
-		switch (faulttype) {
-	    case VM_FAULT_READONLY:
-	      if(!as->segment_table[i].write)
-		{
-	      
-		//throw error
-		}
-	      break;
-	    case VM_FAULT_READ:
-	      
-	      if(!as->segment_table[i].read)
-		{
-	      
-		//throw error
-		}
-	    case VM_FAULT_WRITE:
-	      if(!as->segment_table[i].write)
-		{
-	      
-		//throw error
-		}
-		break;
-	    default:
-		return EINVAL;
-	}
-		  }
-		
-	      }
-	    
-	  }
-	
-	    if(!as->page_table[faultaddress >> 12].is_valid) 
-	      {
-		
-		as->page-table[faultaddress >> 12].index = alloc_kpages(1);
-		
-	      }
-	
-     switch (faulttype) {
-	    case VM_FAULT_READONLY:
-	      break;
-	    case VM_FAULT_READ:
-	    case VM_FAULT_WRITE:
-		break;
-	    default:
-		return EINVAL;
-	}
+    struct addrspace * as = proc_getas();
+    if (as == NULL) {
+        /*
+         * No address space set up. This is probably also a
+         * kernel fault early in boot.
+        */
+        return EFAULT;
+    }
+    
+    int i;
+    for(i=0;i<4;i++) {
+        if(faultaddress >= as->segment_table[i].start && faultaddress < as->segment_table[i].end) {
+            if(!as->ignore_permissions) {
+                switch (faulttype) {
+                    //should probably throw an error somehow instead of kassert
+                    case VM_FAULT_READONLY:
+                        KASSERT(as->segment_table[i].write && as->page_table[faultaddress >> 12].valid);
+                        break;
+                    case VM_FAULT_READ:
+                        KASSERT(as->segment_table[i].read);
+                        break;
+                    case VM_FAULT_WRITE:
+                        KASSERT(as->segment_table[i].write);
+                        break;
+                    default:
+                        return EINVAL;
+                }
+            }
+        }
+    }
+    
+    if(!as->page_table[faultaddress >> 12].valid) {
+        as->page_table[faultaddress >> 12].index = alloc_kpages(1);
+        //STEP 3: put reverse lookup entry in coremap
+    }
+    
+    unsigned int flags = TLBLO_VALID;
+    switch (faulttype) {
+        case VM_FAULT_READONLY:
+            i = tlb_probe(faultaddress & TLBHI_VPAGE, 0);
+            KASSERT(i >= 0);
+            tlb_write(faultaddress & TLBHI_VPAGE, (as->page_table[faultaddress >> 12].index << 12)|TLBLO_DIRTY|TLBLO_VALID, i);
+            break;
+        case VM_FAULT_WRITE:
+            flags = flags | TLBLO_DIRTY;
+        case VM_FAULT_READ:
+            i = tlb_probe(faultaddress & TLBHI_VPAGE, 0);
+            if(i >= 0)
+                tlb_write(faultaddress & TLBHI_VPAGE, (as->page_table[faultaddress >> 12].index << 12)|flags, i);
+            else
+                tlb_random(faultaddress & TLBHI_VPAGE, (as->page_table[faultaddress >> 12].index << 12)|flags);
+            break;
+        default:
+            return EINVAL;
+    }
+    return 0;
 }
-*/
+
